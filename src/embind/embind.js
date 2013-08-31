8202aeca5f9a3cf952ae3029f20ec01fb2260e46
Chad Austin <chad@imvu.com>
2013-08-30 18:25:52 -0700

    IE11 compatibility

--- src/embind/embind.js
+++ src/embind/embind.js
@@ -506,7 +506,7 @@ function new_(constructor, argumentList) {
      * isn't very helpful.  Using IMVU.createNamedFunction addresses the issue.  Doublely-unfortunately, there's no way
      * to write a test for this behavior.  -NRD 2013.02.22
      */
-    var dummy = createNamedFunction(constructor.name, function(){});
+    var dummy = createNamedFunction(constructor.name || 'unknownFunctionName', function(){});
     dummy.prototype = constructor.prototype;
     var obj = new dummy;
 
