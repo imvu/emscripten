479a6074847277d52c7dc562dfc3d40a3f1502c8
Chad Austin <chad@chadaustin.me>
2014-07-13 02:25:36 -0700

    avoid a name conflict with a built-in fastcomp/asm.js import of 'global'

--- src/embind/emval.js
+++ src/embind/emval.js
@@ -183,11 +183,11 @@ var LibraryEmVal = {
   },
 
   // appease jshint (technically this code uses eval)
-  $global: function() { return (function(){return Function;})()('return this')(); },
-  _emval_get_global__deps: ['_emval_register', '$getStringOrSymbol', '$global'],
+  $emval_get_global: function() { return (function(){return Function;})()('return this')(); },
+  _emval_get_global__deps: ['_emval_register', '$getStringOrSymbol', '$emval_get_global'],
   _emval_get_global: function(name) {
     name = getStringOrSymbol(name);
-    return __emval_register(global()[name]);
+    return __emval_register(emval_get_global()[name]);
   },
 
   _emval_get_module_property__deps: ['$getStringOrSymbol', '_emval_register'],
